8eb84b97a42c05a089038842018cc79a6fe57563
shamilslk <shamil.slk@gmail.com>
2026-03-15 13:11:37 +0530

    Implement program counter with load and increment logic

--- src/program_counter.v
+++ src/program_counter.v
@@ -1,1 +1,19 @@
+module program_counter(
+    input clk,
+    input reset,
+    input pc_load,
+    input [7:0] pc_next,
+    output reg [7:0] pc
+);
 
+always @(posedge clk or posedge reset)
+begin
+    if(reset)
+        pc <= 8'd0;
+    else if(pc_load)
+        pc <= pc_next;
+    else
+        pc <= pc + 1;
+end
+
+endmodule
