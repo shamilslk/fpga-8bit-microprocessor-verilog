a634ce191648b331b5793b8027312599592171aa
shamilslk <shamil.slk@gmail.com>
2026-03-15 13:30:23 +0530

    Refactor flag_register module formatting and structure

--- src/flag_register.v
+++ src/flag_register.v
@@ -1,24 +1,31 @@
 module flag_register(
-    input clk,
-    input reset,
-    input zero_in,
-    input carry_in,
-    input sign_in,
-    output reg zero,
-    output reg carry,
-    output reg sign
+
+input clk,
+input reset,
+
+input zero_in,
+input carry_in,
+input sign_in,
+
+output reg zero,
+output reg carry,
+output reg sign
+
 );
 
-always @(posedge clk or posedge reset) begin
-    if(reset) begin
-        zero  <= 0;
+always @(posedge clk or posedge reset)
+begin
+    if(reset)
+    begin
+        zero <= 0;
         carry <= 0;
-        sign  <= 0;
+        sign <= 0;
     end
-    else begin
-        zero  <= zero_in;
+    else
+    begin
+        zero <= zero_in;
         carry <= carry_in;
-        sign  <= sign_in;
+        sign <= sign_in;
     end
 end
 
