5772f5f4a491057e3f28217d6097f7d5d9e1a555
shamilslk <shamil.slk@gmail.com>
2026-03-15 13:20:03 +0530

    Implement flag register with reset and input signals

--- src/flag_register.v
+++ src/flag_register.v
@@ -1,1 +1,25 @@
+module flag_register(
+    input clk,
+    input reset,
+    input zero_in,
+    input carry_in,
+    input sign_in,
+    output reg zero,
+    output reg carry,
+    output reg sign
+);
 
+always @(posedge clk or posedge reset) begin
+    if(reset) begin
+        zero  <= 0;
+        carry <= 0;
+        sign  <= 0;
+    end
+    else begin
+        zero  <= zero_in;
+        carry <= carry_in;
+        sign  <= sign_in;
+    end
+end
+
+endmodule
