358038f7832d59feb184d41b4a24457ed52b64e0
shamilslk <shamil.slk@gmail.com>
2026-03-15 13:29:36 +0530

    Implement data memory module with read/write functionality

--- src/data_memory.v
+++ src/data_memory.v
@@ -1,1 +1,27 @@
+module data_memory(
 
+input clk,
+input mem_write,
+input mem_read,
+
+input [7:0] address,
+input [7:0] write_data,
+
+output reg [7:0] read_data
+
+);
+
+reg [7:0] ram [0:255];
+
+always @(posedge clk)
+begin
+
+    if(mem_write)
+        ram[address] <= write_data;
+
+    if(mem_read)
+        read_data <= ram[address];
+
+end
+
+endmodule
